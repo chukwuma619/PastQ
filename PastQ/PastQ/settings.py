a02702f828f31de3e88cf4f501290b0c4b962ce3
Chukwuma Ebube <chukwuma619@gmail.com>
2023-05-31 00:04:49 +0100

    collectstatic

--- PastQ/PastQ/settings.py
+++ PastQ/PastQ/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 import environ
+import os
 
 env = environ.Env()
 
@@ -141,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
